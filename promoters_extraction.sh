9e8540f8290e7f00f7b52d56f6d0e529e8606bd8
Ammar Abdalrahem <ammar.abdalrahem@outlook.com>
2022-04-18 02:33:14 +0200

    Update promoters_extraction.sh

--- promoters_extraction.sh
+++ promoters_extraction.sh
@@ -17,35 +17,40 @@ ls -1 *.gene.gff3|
     sort -u |
     while read i
     do
-        perl -lane 'if($F[2] eq "gene" && $F[8]=~/ID=([^;]+)/){ ($id,$sta,$end,$str)=($1,$F[3],$F[4],$F[6]);
-            if($str eq "+"){ $sta-=500; if($sta<0){ $sta=0 } } else { $end+=500 }  
-            print "$F[0]\t$sta\t$end\t$id\t0\t$str" } ' $i> $i.bed 
-    done 
+        perl -lane 'if($F[2] eq "gene" && $F[8]=~/ID=([^;]+)/){ ($id,$sta,$end,$str)=($1,$F[3],$F[4],$F[6]);if($str eq "+"){$end=$sta; $sta-=500;
+        if($sta<0){ $sta=0 }} else { $sta=$end; $end+=500 } print "$F[0]\t$sta\t$end\t$id\t0\t$str" }' $i > $i.bed
+    done
 
 
 #change the extention name to be as fasta file name plus .bed for easy run in bedtools
 
-for f in ./*.bed; do      mv -- "$f" "${f%.*.1.gene.gff3.bed}.fa.bed"; done
+for f in ./*.bed; do  mv -- "$f" "${f%.*.1.gene.gff3.bed}.fa.sm.fna.bed"; done
 
 
-#flank coordinates within (bedtools slop uning fasta file) to slove *the beyond the length)error make length not more than chr. size 
-ls -1 *.fa|
+#flank coordinates within (bedtools slop uning fasta file) to slove (the beyond the length) error make length not more than chr. size
+ls -1 *.fna|
     sort -u |
     while read i
     do
 
-	perl -lne 'if(/^>(\S+)/){ $id=$1 } else { $len{$id} += length($_) } END{ foreach $id (sort keys(%len)){ print "$id\t$len{$id}" } }' $i> $i.chr.genome
-	bedtools slop -l 0 -r 0 -s -i $i.bed -g $i.chr.genome > $i.flank.bed
-done
+        perl -lne 'if (/^>(\S+)/){ $id=$1 } else { $len{$id} += length($_) } END{ foreach $id (sort keys(%len)){ print "$id\t$len{$id}" } }' $i > $i.chr.genome
+        bedtools slop -l 0 -r 0 -s -i $i.bed -g $i.chr.genome > $i.flank.bed
+
+    done
 
 
 #Run bedtools for promoter sequence extraction from bed file coordinates
 #input -fi fasta file -bed bed flank file, output -fo name new file
-ls -1 *.fa|
+
+ls -1 *.fna|
     sort -u |
     while read i
     do
 
-bedtools getfasta -fi $i -bed $i.flank.bed -s -fo $i.promotor.out
+bedtools getfasta -fi $i -bed $i.flank.bed -s -name -fo $i.pr.fa
 
-done
+    done
+
+#in sankemake add dirctory for outputs
+rm -r *.genome  *.bed  *.fai
+
