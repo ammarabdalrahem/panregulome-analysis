e0c31066f5f9b77162b58c7a34ed3c78d47b0d5c
Ammar Abdalrahem <ammar.abdalrahem@outlook.com>
2022-04-18 12:14:29 +0200

    Create assembly_assessment.sh

---
+++ src/scripts/assembly_assessment.sh
@@ -0,0 +1,24 @@
+#!usr/bin/bash
+
+
+#analysis stats for assembly files 
+#assembly-stats get ssembly statistics from FASTA and FASTQ files.
+assembly-stats -u out/uncom_data/Bdistachyon*.fa|cut -f 1-2,7,8> stats.csv
+
+#convert output to CSV file
+cat stats.csv |tr "\\t" "," > stats_table.csv
+
+#loop for count and cut genes numbers  
+for i in out/uncom_data/Bdistachyon*.gff3; do
+  sed -n '3p' $i | cut -d ' ' -f 3,4 && cat $i| grep -v "#" | cut -f3|sort | uniq -c|grep "gene" |grep  -o '[[:digit:]]*'
+  done | paste -sd "\t\n" > genes.csv
+
+#cut only gene numbers 
+cut -d" " -f2 genes.csv |awk '{print $2}' > genes_table.csv
+
+#join two tables  
+paste -d , stats_table.csv genes_table.csv > assessment_table.csv
+
+#clear data
+mv assessment_table.csv  tables/
+rm genes_table.csv genes.csv stats_table.csv stats.csv
