15c5a591ec3241e74eaab90b46c6b5903506efcb
Ammar Abdalrahem <ammar.abdalrahem@outlook.com>
2022-12-01 10:19:27 +0100

    Create data_sorting.sh

---
+++ src/scripts/data_sorting.sh
@@ -0,0 +1,8 @@
+mkdir out/uncom_data/cds
+mv  out/uncom_data/*cds*.fa out/uncom_data/cds/
+
+mkdir out/uncom_data/genomes
+mv  out/uncom_data/*.fa out/uncom_data/genomes/
+
+mkdir out/uncom_data/gff3
+mv  out/uncom_data/*.gff3 out/uncom_data/gff3/
