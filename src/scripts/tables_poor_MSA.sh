93614c4afc999928d89a38a5b375feef754bd5fc
Ammar Abdalrahem <ammar.abdalrahem@outlook.com>
2023-12-18 10:54:34 +0100

    Update tables_poor_MSA.sh

--- src/scripts/tables_poor_MSA.sh
+++ src/scripts/tables_poor_MSA.sh
@@ -1,1 +1,5 @@
+#to create tables/total_seq_cluster_pr.csv for calculation poor alignment 
 for file in out/uncom_data/promoter_cluster/*; do echo -n "$(basename "$file"),"; grep ">" "$file" | wc -l; done > tables/total_seq_cluster_pr.csv
+
+#to create tables/total_seq_cluster_cds.csv for calculation poor alignment 
+for file in out/uncom_data/cds_cluster/*; do echo -n "$(basename "$file"),"; grep ">" "$file" | wc -l; done > tables/total_seq_cluster_cds.csv
